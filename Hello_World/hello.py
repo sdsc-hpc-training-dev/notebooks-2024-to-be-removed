a6022269106625d809bf32364da7436fec677055
marypthomas <marypthomas88@gmail.com>
2021-07-20 14:59:45 -0700

    update hello.py

---
+++ Hello_World/hello.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Define a function
+def world(name):
+   print("Hello, " + name) 
+
+# Define a function
+def world2(name):
+   print("Hello2, " + name) 
+
+
+def greeting(name):
+   print("Greetings, " + name)
+
+
